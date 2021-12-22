066438c39ae4b8a928349e16b40fc64b37d47765
Jruff1215 <Jamie.Ruffin@bryanuniversity.edu>
2021-12-21 19:15:03 -0500

    Crud Store Part 2

--- week1/crudStore/CrudServer.js
+++ week1/crudStore/CrudServer.js
@@ -1,6 +1,11 @@
 const express = require('express');
 const app = express();
 const mongoose = require('mongoose')
+const morgan = require('morgan')
+
+app.use(express.json())
+app.use(morgan('dev'))
+
 mongoose.connect('mongodb://localhost:27017/storedb', 
 {
     useNewUrlParser: true,
@@ -10,9 +15,18 @@ mongoose.connect('mongodb://localhost:27017/storedb',
   },
   () => console.log("Connected to the DB")
 )
+const inventoryRouter = require("./routes/inventoryRouter.js")
+// console.log(inventoryRouter)
+app.use("/inventories", require("./routes/inventoryRouter.js"))
+
+app.use((err, req, res, next) => {
+    console.log(err)
+    return res.send({errMsg: err.Message})
+})
 app.get('/', (req, res) => {
     res.send("Successful")
 })
 app.listen(9000, () => {
-    console.log("The App is listening on port 9000.")
+    console.log("The Server is listening on port 9000.")
 });
+
---
+++ week1/crudStore/routes/inventoryRouter.js
@@ -0,0 +1,63 @@
+const express = require('express')
+const inventory = require('../models/inventory.js')
+const inventoryRouter = express.Router()
+const Inventory = require('../models/inventory.js')
+
+inventoryRouter.get("/", (req, res, next) => {
+        Inventory.find((err, inventories) => {
+        if(err){
+            console.log("err")
+            res.status(500)
+            return next(err)
+        }
+        return res.status(200).send(inventories)
+    })
+})
+
+inventoryRouter.get("/:inventoryId", (req, res, next) => {
+    Inventory.findOne({_id: req.params.inventoryId}, (err, foundItem) => {
+        if(err) {
+            console.log("err")
+            res.status(500)
+            return next(err)
+        }
+        return res.status(201).send(foundItem)
+    })
+})
+
+inventoryRouter.post("/", (req, res, next) => {
+    console.log(req.body)
+    const newInventory = new Inventory(req.body)
+    newInventory.save((err, savedInventory) => {
+        if(err){
+            res.status(500)
+            return next(err)
+        }
+        return res.status(201).send(savedInventory)
+    })
+})
+
+inventoryRouter.delete("/:inventoryId", (req, res, next) => {
+    Inventory.findOneAndDelete({_id: req.params.inventoryId}, (err, deletedItem) => {
+        if(err){
+            res.status(500)
+            return next(err)
+        }
+        return res.status(200).send('Successfully Deleted Item')
+    })
+})
+
+inventoryRouter.put("/:inventoryId", (req, res, next) => {
+    Inventory.findOneAndUpdate({_id: req.params.inventoryId},
+        req.body, 
+        { new: true }, 
+        (err, updatedMovie) => {
+            if(err) {
+                res.status(500)
+                return next(err)
+            }
+            return res.status(201).send(updatedMovie)
+        })
+})
+
+module.exports = inventoryRouter
